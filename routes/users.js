632602d669d8d9cd567de47195ab3723c34205b0
justindho <justinho515@gmail.com>
2020-03-28 18:26:06 -0700

    Save changes before merge

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+    var userListing = {};
+
+    dbo.collection("users").find({}).toArray( (err, result) => {
+        if (err) throw err;
+
+        userListing = result;
+        console.log(result);
+        
+        db.close();
+
+    });
+
+    // res.send('respond with a resource');
+    res.send(result);
 });
+
+router.get('/')
 
 module.exports = router;
