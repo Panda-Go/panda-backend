cd147dd6fcb917d627ce625e6d9e068de64fcc76
Param Bole <pbole@usc.edu>
2020-03-28 19:01:00 -0700

    - Panda Mongo Connections

--- routes/panda.js
+++ routes/panda.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var config = require('./config');
+var dbo;
+var pandas;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+// Connect to the PandaGO MongoDB
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect(config.dbURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err, db) => {
+  if (err) throw err;
+  dbo = db.db("PandaGoDB");
+  pandas = dbo.collection("pandas");
+});
+
+
+
+
+/* create new panda. */
+router.post('/create', function(req, res, next) {
+  // Sample Test Query to DB
+  const body = req.body;
+  pandas
+      .insertOne(
+          {
+            _id: body.phoneNumber, weapon: "N/A", points:"1000", lat: body.lat , lng: body.lng
+          });
+  res.status(200).send()
 });
 
 module.exports = router;
